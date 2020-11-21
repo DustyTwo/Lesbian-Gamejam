c6161fefac4433ffa6729e6871f0c73d1bdb7262
Nathalie <nathaliet1099@gmail.com>
2020-11-22 00:41:52 +0100

    0.9 swords and shields minigame

---
+++ Assets/FeatureTesting/NattaTemp/SwordCursorScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCursorScript : MonoBehaviour
+{
+    Camera _mainCamera;
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = GetMouseWorldPosition();
+    }
+
+    Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, _mainCamera);
+        vec.z = 0;
+        return vec;
+    }
+
+    Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
---
+++ Assets/FeatureTesting/NattaTemp/SwordMoveTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordMoveTarget : MonoBehaviour
+{
+    Camera _mainCamera;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _speed;
+
+    private Vector2 _move;
+
+    private void Awake()
+    {
+        _move = _player.transform.position - transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(_move * _speed * Time.fixedDeltaTime);
+    }
+}
